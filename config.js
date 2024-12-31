5b81f6544713e3f7f974b0635575e39b442a000e
unknown <agilauliarahmanm@gmail.com>
2024-12-31 23:50:35 +0700

    first

---
+++ config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDHtAwiR6MoAhrsGYoy5S5W99c16omWVXY",
+    authDomain: "blind-project-2a7b5.firebaseapp.com",
+    databaseURL: "https://blind-project-2a7b5-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "blind-project-2a7b5",
+    storageBucket: "blind-project-2a7b5.firebasestorage.app",
+    messagingSenderId: "575658475500",
+    appId: "1:575658475500:web:047a3e2f81e65321760ac9",
+    measurementId: "G-T76R04JV40"
+  };
+
+    export const authCredentials = {
+    email: "agilauliarahmanm@gmail.com",
+    password: "vipranz160205",
+    };
